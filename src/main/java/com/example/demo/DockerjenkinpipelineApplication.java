b2ec48fa7e2979f84a19f685252593c65a5200a7
Anand <you@example.com>
2020-09-02 14:41:11 +0100

    first commit

---
+++ src/main/java/com/example/demo/DockerjenkinpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinpipelineApplication.class, args);
+	}
+
+}
